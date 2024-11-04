0ba923057295037c3bada216671197ad5b096ac0
Denis Kuryshov <dkuryshov@Deniss-Laptop.local>
2024-11-04 16:45:41 +0400

    Initial commit for Jenkins CI/CD demo

---
+++ test/app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+});
+
+test('GET /', async () => {
+  const res = await request(app).get('/');
+  expect(res.statusCode).toEqual(200);
+  expect(res.text).toEqual('Hello World!');
+});
+
